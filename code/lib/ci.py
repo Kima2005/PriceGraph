e471c7eb73f3f1cfc7aca9a2d8bb914e0689f48c
Vuong Khang Minh <114381643+Kima2005@users.noreply.github.com>
2024-09-28 03:49:15 +0000

    Pending changes exported from your codespace

--- code/lib/ci.py
+++ code/lib/ci.py
@@ -17,17 +17,17 @@ def collective_influence(G, u=None, distance=2):
         path_lengths = nx.single_source_shortest_path_length(
             G, source=node, cutoff=distance
         )
-        print(f"Node: {node}")
-        print(f"Path lengths from node {node}: {path_lengths}")
+        # print(f"Node: {node}")
+        # print(f"Path lengths from node {node}: {path_lengths}")
         frontier_nodes = {
             node for node, path_length in path_lengths.items()
             if path_length == distance
         }
-        print(f"Frontier nodes at distance {distance} from node {node}: {frontier_nodes}")
+        # print(f"Frontier nodes at distance {distance} from node {node}: {frontier_nodes}")
         node_ci = reduced_degree(node) * sum(map(reduced_degree, frontier_nodes))
-        print(f"Reduced degree of node {node}: {reduced_degree(node)}")
-        print(f"Sum of reduced degrees of frontier nodes: {sum(map(reduced_degree, frontier_nodes))}")
-        print(f"Collective influence of node {node}: {node_ci}")
+        # print(f"Reduced degree of node {node}: {reduced_degree(node)}")
+        # print(f"Sum of reduced degrees of frontier nodes: {sum(map(reduced_degree, frontier_nodes))}")
+        # print(f"Collective influence of node {node}: {node_ci}")
         collective_influence[node] = node_ci
     if u is not None:
         return collective_influence[u]
